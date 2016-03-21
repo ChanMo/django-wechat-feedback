dbbf1594dd3be2072b3ece6ab107ca31c28a5aec
ChanMo <chen.orange@aliyun.com>
2016-03-21 15:38:54 +0800

    update

--- feedback/admin.py
+++ feedback/admin.py
@@ -3,5 +3,6 @@ from .models import Feedback
 
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('member', 'type', 'content', 'is_view', 'created')
+    list_filter = ('created', 'is_view')
 
 admin.site.register(Feedback, FeedbackAdmin)
