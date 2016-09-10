25b3f1580e33993a7a5074de37e535089a36807c
ChanMo <chen.orange@aliyun.com>
2016-09-10 16:57:24 +0800

    rename to django-wechat-feedback, rm useless dirs

--- build/lib/feedback/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Feedback
-
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('member', 'type', 'content', 'is_view', 'created')
-    list_filter = ('created', 'is_view')
-
-admin.site.register(Feedback, FeedbackAdmin)
--- build/lib/feedback/apps.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# vim: set fileencoding=utf-8 :
-from django.apps import AppConfig
-
-class FeedbackConfig(AppConfig):
-    name = u'feedback'
-    verbose_name = u'反馈'
